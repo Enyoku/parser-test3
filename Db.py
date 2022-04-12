2e51945eeaafba28ac3346e78a4d6b42ddc183a1
Enyoku <91157316+Enyoku@users.noreply.github.com>
2022-04-13 00:37:16 +0300

    fix 2.0

--- Db.py
+++ Db.py
@@ -1,36 +1,13 @@
-from sqlalchemy import Table, Index, Integer, String, Column, Text, \
-    DateTime, Boolean, PrimaryKeyConstraint, \
-    UniqueConstraint, ForeignKeyConstraint, ForeignKey, Numeric, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-
+from sqlalchemy import create_engine
 from local_settings import posrtgesql as settings
+
 # ?=======================================IF USES PostgreSQL=============================================================
 
-engine = create_engine(
-    f'postgresql+psycopg2://{settings["username"]}:{settings["password"]}@{settings["host"]}/{settings["database"]}', echo=True,)
+# engine = create_engine(
+#     f'postgresql+psycopg2://{settings["username"]}:{settings["password"]}@{settings["host"]}/{settings["database"]}', echo=True,)
 
 # ?========================================IF USES SQLite================================================================
 
-# engine = create_engine('sqlite:///places.sqlite', echo=True)
+engine = create_engine('sqlite:///places.sqlite', echo=True)
 
 # ?======================================================================================================================
-
-DeclarativeBase = declarative_base()
-
-
-class Product(DeclarativeBase):
-    __tablename__ = 'Product'
-
-    product_id = Column(Integer, primary_key=True)
-    product_name = Column(String(100), nullable=False)
-    category_name = Column(String(100))
-    check_date = Column(DateTime)
-    description = Column(Text)
-    article = Column(Integer)
-    price = Column(Numeric(10, 2), nullable=False)
-    Category = relationship("Category")
-    last_check = relationship("Last_check")
-
-
-DeclarativeBase.metadata.create_all(engine)
